509bbe2d6aa5af2bcddbfabf544e3540881b8dc3
Cesar Augusto  Gomes (a94448) <cacgs1@iscte-iul.pt>
2025-10-01 03:49:30 +0100

    Código do módulo m1

---
+++ m1.java
@@ -0,0 +1,150 @@
+class m1 {
+
+    int next(int n) {
+        return n + 1;
+    }
+
+    int previous(int n) {
+        return n - 1;
+    }
+
+    double doubleOf(double n) {
+        return 2 * n;
+    }
+
+    int square(int n) {
+        return n * n;
+    }
+
+    int difference(int a, int b) {
+        return a - b;
+    }
+
+    double average(double a, double b) {
+        return (double) (a + b) / 2;
+    }
+
+    double average(int a, int b) {
+        return (double) (a + b) / 2;
+    }
+
+    int round(double n) {
+        return (int) (n + 0.5);
+    }
+
+    boolean isNegative(int x) {
+        return x < 0;
+    }
+
+    boolean isDigit(int x) {
+        if (x >= 0 && x <= 9)
+            return true;
+        else
+            return false;
+    }
+
+    boolean isEven(int x) {
+        return x % 2 == 0;
+    }
+
+    boolean isOdd(int x) {
+        return x % 2 != 0;
+    }
+
+    double abs(double x) {
+        return (x > 0) ? x : -x;
+    }
+
+    // Solução Alternativa :
+    //
+    // double abs(double x) {
+    //   return n < 0 ? -x : x;
+    // }
+    double min(double a, double b) {
+        return (a < b) ? a : b;
+    }
+
+    // solução alternativa :
+    //
+    //double min(double a, double b) {
+    //    if (a < b)
+    //        return a;
+    //    else
+    //        return b;
+    //}
+    int min(int a, int b) {
+        if (a < b)
+            return a;
+        else
+            return b;
+    }
+
+    double abs(double n) {
+        if (n > 0)
+            return n;
+        else
+            return -n;
+    }
+
+    int max(int a, int b) {
+        if (a > b)
+            return a;
+        else
+            return b;
+    }
+
+    int irsgroup(int rendimento) {
+        if (rendimento < 10000) {
+            return 1;
+        } else if (rendimento >= 10000 && rendimento <= 25000) {
+            return 2;
+        } else if (rendimento > 25000 && rendimento <= 48500) {
+            return 3;
+        } else {
+            return 4;
+        }
+    }
+
+    double irsTax(int escalao) {
+        if (escalao == 1) {
+            return 0.12;
+        } else if (escalao == 2) {
+            return 0.18;
+        } else if (escalao == 3) {
+            return 0.23;
+        } else if (escalao == 4) {
+            return 0.29;
+        } else {
+            return 0.0;
+        }
+    }
+}
+
+class IrsUtil {
+
+    static int irsGroup(int rendimento) {
+        if (rendimento < 10000) {
+            return 1;
+        } else if (rendimento >= 10000 && rendimento <= 25000) {
+            return 2;
+        } else if (rendimento > 25000 && rendimento <= 48500) {
+            return 3;
+        } else {
+            return 4;
+        }
+    }
+
+    static double irsTax(int escalao) {
+        if (escalao == 1) {
+            return 0.12;
+        } else if (escalao == 2) {
+            return 0.18;
+        } else if (escalao == 3) {
+            return 0.23;
+        } else if (escalao == 4) {
+            return 0.29;
+        } else {
+            return 0.0;
+        }
+    }
+}
