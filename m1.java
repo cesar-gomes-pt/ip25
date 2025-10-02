51080827f9eb26629a683ae8d7481c15a2613f5a
Cesar Augusto  Gomes (a94448) <cacgs1@iscte-iul.pt>
2025-10-02 01:06:51 +0100

    Código do módulo m1

--- m1.java
+++ m1.java
@@ -172,6 +172,10 @@ class m1 {
     boolean sameSign(int a, int b) {
         return (a < 0 && b < 0 || a > 0 && b > 0);
     }
+
+    boolean onlyOnePositive(int a, int b) {
+        return ((a > 0 && b < 0) || (a < 0 && b > 0));
+    }
 }
 
 class IrsUtil {
