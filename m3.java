1a078f640af28e021a24bb8075a0f4a10b2ab296
Cesar Augusto  Gomes (a94448) <cacgs1@iscte-iul.pt>
2025-10-27 19:03:52 +0000

    Código do módulo m3

--- m3.java
+++ m3.java
@@ -12,3 +12,22 @@ class EvenOdd {
         return !even;
     }
 }
+
+class MathUtil {
+
+    static double floor(double d) {
+        return (int) d;
+    }
+
+    static double decimalPart(double d) {
+        if (d >= 0) {
+            // ex.: 4.4 -> 0.4 ou 13.0 -> 0.0
+            return d - floor(d);
+        } else {
+            // ceil(d) = -floor(-d)
+            double ceil = -floor(-d);
+            // ex.: -1.5 -> -0.5 ou -0.5 -> -0.5
+            return d - ceil;
+        }
+    }
+}
