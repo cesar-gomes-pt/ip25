0f68ac6040657e3f3c3780fae37845b89773db28
Cesar Augusto  Gomes (a94448) <cacgs1@iscte-iul.pt>
2025-10-29 22:00:03 +0000

    Código do módulo m5

--- m5.java
+++ m5.java
@@ -1,9 +1,20 @@
 class m5 {
 
     static void incrementation() {
+        // A seguinte sequência de instruções faz com que a variável i tome os valores 1, 2, 3, 4.
+        // Altere o código por forma a manter este comportamento utilizando a instrução de incrementação (++) :
         int i = 1;
         i++;
         i++;
         i++;
+    }
+
+    static void cumulativeAssign() {
+        // A seguinte sequência de instruções faz com que a variável n tome os valores 1, 3, 6, 10.
+        // Altere o código por forma a manter este comportamento utilizando a instrução de atribuição cumulativa (+=) :
+        int n = 1;
+        n += 2;
+        n += 3;
+        n += 4;
     }
 }
